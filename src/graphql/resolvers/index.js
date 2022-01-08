5b6fd485ab7daebda6a6fb2e80f68cf58b263f85
rafalstrozyk <rafal.strozyk.biz@gmail.com>
2022-01-08 13:10:18 +0100

    testing querys, mutations

--- src/graphql/resolvers/index.js
+++ src/graphql/resolvers/index.js
@@ -1,6 +1,34 @@
+import Post from '../../models/post.model';
+
 const resolvers = {
   Query: {
     hello: () => 'hello world!',
+    getAllPosts: async () => Post.find(),
+    getPost: async (parent, { id }) => Post.findById(id),
+  },
+  Mutation: {
+    createPost: async (parent, args) => {
+      const { title, description } = args.post;
+      const post = new Post({ title, description });
+      await post.save();
+      return post;
+    },
+
+    deletePost: async (parent, { id }) => {
+      await Post.findByIdAndDelete(id);
+      return 'Success delete post!';
+    },
+
+    updatePost: async (parent, args) => {
+      const { title, description } = args.post;
+      const { id } = args;
+      const post = await Post.findByIdAndUpdate(
+        id,
+        { title, description },
+        { new: true },
+      );
+      return post;
+    },
   },
 };
 
